cca18377617e80e382c2e2bf78ececa109e8f45d
Ansh13Patel <66872036+Ansh13Patel@users.noreply.github.com>
2020-10-25 11:13:49 +0530

    player and enemy setup

---
+++ Finding Key/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public AIPath enemyAI;
+    public Animator enemyAnim;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(enemyAI.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            enemyAnim.SetBool("Walking", true);
+        } else if(enemyAI.desiredVelocity.x <= -0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            enemyAnim.SetBool("Walking",true);
+        }
+
+       if(enemyAI.desiredVelocity.x < 0.01f && enemyAI.desiredVelocity.x < -0.01f)
+        {
+            enemyAnim.SetBool("Walking", false);
+        }
+    }
+}
---
+++ Finding Key/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float moveSpeed = 3f;
+    public Animator anim;
+    private Vector2 move;
+
+
+    private void Update()
+    {
+        playermove();
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.AddForce(move * moveSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+    }
+
+    void playermove()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        if (Mathf.Abs(x) <= 0.05f)
+        {
+            rb2d.velocity = new Vector2(0, y);
+            anim.SetBool("Walking", false);
+        }
+        if (Mathf.Abs(y) <= 0.05f)
+        {
+            rb2d.velocity = new Vector2(x, 0);
+            anim.SetBool("Walking", false);
+        }
+
+        if(rb2d.velocity != Vector2.zero)
+        {
+            anim.SetBool("Walking", true);
+        }
+       
+
+        move = new Vector2(x, y);
+    }
+}
